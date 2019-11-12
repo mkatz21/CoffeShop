c068722cea8f98099b8984c9fb84b25d2efafc66
katzm <markatz@umich.edu>
2019-11-12 09:41:00 -0500

    Added a comment

--- app/src/main/java/com/example/coffeshop/MainActivity.java
+++ app/src/main/java/com/example/coffeshop/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    //Added a comment
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
