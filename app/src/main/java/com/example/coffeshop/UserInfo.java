b71cd16de874e8760a296fb4c2932eed08fd4512
katzm <markatz@umich.edu>
2019-11-14 08:55:51 -0500

    Added User Info Class

---
+++ app/src/main/java/com/example/coffeshop/UserInfo.java
@@ -0,0 +1,29 @@
+package com.example.coffeshop;
+
+import android.media.Image;
+
+public class UserInfo {
+
+    public String userInfoName;
+    public String userInfoPassword;
+    public String userInfoFirstName;
+    public String userInfoLastName;
+    public String userInfoEmail;
+    public String userInfoHomeZipcode;
+    public String userInfoCurrentZipcode;
+    public Image userInfoImage;
+
+    public UserInfo() {
+    }
+
+    public UserInfo(String userInfoName, String userInfoPassword, String userInfoFirstName, String userInfoLastName, String userInfoEmail, String userInfoHomeZipcode, String userInfoCurrentZipcode, Image userInfoImage) {
+        this.userInfoName = userInfoName;
+        this.userInfoPassword = userInfoPassword;
+        this.userInfoFirstName = userInfoFirstName;
+        this.userInfoLastName = userInfoLastName;
+        this.userInfoEmail = userInfoEmail;
+        this.userInfoHomeZipcode = userInfoHomeZipcode;
+        this.userInfoCurrentZipcode = userInfoCurrentZipcode;
+        this.userInfoImage = userInfoImage;
+    }
+}
