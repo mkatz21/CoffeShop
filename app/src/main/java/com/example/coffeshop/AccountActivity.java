1de1319b3c7b8b23d2f38a6a1c57e77132017191
katzm <markatz@umich.edu>
2019-11-14 11:31:52 -0500

    Added all activities as empty activities

--- app/src/main/java/com/example/coffeshop/AccountActivity.java
+++ app/src/main/java/com/example/coffeshop/AccountActivity.java
@@ -4,14 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
-
-    //Added a comment
+public class AccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_account);
     }
 }
---
+++ app/src/main/java/com/example/coffeshop/HomeActivity.java
@@ -0,0 +1,17 @@
+package com.example.coffeshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+
+    //Added a comment
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
