c25c87b85c27cbea2b5b4b112d02d8931f73c365
Sam <epstein.sam@gmail.com>
2019-11-18 22:09:08 -0500

    SIE 11.18.19 Added first draft of the CoffeeShopActivity UI. Includes spinners (dropdowns) which should pull from arrays when the app is functioning

--- app/src/main/java/com/example/coffeshop/CoffeeShopActivity.java
+++ app/src/main/java/com/example/coffeshop/CoffeeShopActivity.java
@@ -3,12 +3,32 @@ package com.example.coffeshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class CoffeeShopActivity extends AppCompatActivity {
+    Spinner spinnerReservationDuration, spinnerReservationTableType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coffee_shop);
+
+spinnerReservationDuration=findViewById(R.id.spinnerAvailabilityDuration);
+spinnerReservationTableType = findViewById(R.id.spinnerAvailabilityTableType);
+
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapterDuration = ArrayAdapter.createFromResource(this,
+                R.array.reservation_duration_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapterDuration.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinnerReservationDuration.setAdapter(adapterDuration);
+
+        ArrayAdapter<CharSequence> adapterTableType = ArrayAdapter.createFromResource(this,
+                R.array.reservation_table_type_array, android.R.layout.simple_spinner_item);
+        adapterTableType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinnerReservationTableType.setAdapter(adapterTableType);
     }
 }
