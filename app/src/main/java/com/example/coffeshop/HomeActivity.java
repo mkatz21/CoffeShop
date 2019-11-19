a577d38acf35c919b3bfd2de9896c293f6d4d73a
katzm <markatz@umich.edu>
2019-11-19 09:15:44 -0500

    Created main menu on the home activity

--- app/src/main/java/com/example/coffeshop/HomeActivity.java
+++ app/src/main/java/com/example/coffeshop/HomeActivity.java
@@ -3,6 +3,8 @@ package com.example.coffeshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -14,4 +16,17 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mainmenu, menu);
+        
+
+        return super.onCreateOptionsMenu(menu);
+    }
 }
+
+
+e
--- app/src/main/java/com/example/coffeshop/MainPaymentActivity.java
+++ app/src/main/java/com/example/coffeshop/MainPaymentActivity.java
@@ -3,6 +3,8 @@ package com.example.coffeshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainPaymentActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class MainPaymentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_payment);
+
+
     }
 }
