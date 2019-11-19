862b663ea3044a21b473802fc0ab6448edb62905
Sam <epstein.sam@gmail.com>
2019-11-19 09:35:33 -0500

    SIE 11.19.19 Adding Menu functionality

--- app/src/main/java/com/example/coffeshop/HomeActivity.java
+++ app/src/main/java/com/example/coffeshop/HomeActivity.java
@@ -1,10 +1,14 @@
 package com.example.coffeshop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -15,6 +19,9 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 
     @Override
@@ -25,6 +32,42 @@ public class HomeActivity extends AppCompatActivity {
 
 
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.MainPayment) {
+
+            Intent mainPaymentIntent = new Intent(this, MainPaymentActivity.class);
+            startActivity(mainPaymentIntent);
+
+        } else if(item.getItemId() == R.id.Home) {
+
+            Toast.makeText(this, "You are already on the Home Page", Toast.LENGTH_SHORT).show();
+
+        } else if(item.getItemId() == R.id.Account) {
+
+            Intent accountIntent = new Intent(this, AccountActivity.class);
+            startActivity(accountIntent);
+
+        } else if(item.getItemId() == R.id.AddPayment) {
+
+            Intent addPaymentIntent = new Intent(this, AddPaymentMethodActivity.class);
+            startActivity(addPaymentIntent);
+        } else if(item.getItemId() == R.id.CheckIn) {
+
+            Intent checkInIntent = new Intent(this, CheckInActivity.class);
+            startActivity(checkInIntent);
+        } else if(item.getItemId() == R.id.CoffeeShop) {
+
+            Intent coffeeShopIntent = new Intent(this, CoffeeShopActivity.class);
+            startActivity(coffeeShopIntent);
+        } else if(item.getItemId() == R.id.SignUp) {
+
+            Intent signupIntent = new Intent(this, SignUpActivity.class);
+            startActivity(signupIntent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
 
