bda321600ecfaf4ce3cf96d59ccc1ec6fe5e51d7
seleneceja <sceja@umich.edu>
2019-11-20 01:55:06 -0500

    Account and SignUp Activity updated pending final bug review.

--- app/src/main/java/com/example/coffeshop/AccountActivity.java
+++ app/src/main/java/com/example/coffeshop/AccountActivity.java
@@ -3,12 +3,26 @@ package com.example.coffeshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class AccountActivity extends AppCompatActivity {
 
+    Button AccountDetails, PastReservations, Payments, Review;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+
+
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+
+
+        AccountDetails = findViewById(AccountDetails);
+        PastReservations = findViewById(PastReservations);
+        Payments = findViewById(Payments);
+        Review = findViewById(Review);
     }
+
 }
--- app/src/main/java/com/example/coffeshop/SignUpActivity.java
+++ app/src/main/java/com/example/coffeshop/SignUpActivity.java
@@ -3,12 +3,92 @@ package com.example.coffeshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import com.google.firebase.auth.FirebaseAuth;
+import android.view.View;
+import android.widget.Toast;
+import android.content.Intent;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.OnCompleteListener;
 
 public class SignUpActivity extends AppCompatActivity {
+
+
+    EditText FirstName, LastName, Email, PhoneNumber, Password, ConfirmPassword;
+    Button Create;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+
+        FirstName = findViewById(R.id.FirstName);
+        LastName = findViewById(R.id.LastName);
+        Email = findViewById(Email);
+        PhoneNumber = findViewById(PhoneNumber);
+        Password = findViewById(Password);
+        ConfirmPassword = findViewById(ConfirmPassword)
+
+        mAuth = FirebaseAuth.getInstance();
     }
+
+    @Override
+    public void onClick(View v) {
+
+        String email = Email.getText().toString();
+        String password = Password.getText().toString();
+
+        if (v==Create) {
+
+            makeNewUsers(email,password);
+        } else if (v==Create) {
+
+
+            mAuth.signInWithEmailandPassword(email, password)
+                    .addOnCompleteListener(this,
+            if (task.isSuccessful()) {
+
+                Toast.makeText(SignUpActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+
+                Intent logoutIntent = new Intent (SignUpActivity.this, AccountActivity.class);
+                startActivity(logoutIntent);}
+
+            else {
+
+                Toast.makeText(SignUpActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+
+
+        public void makeNewUsers (String email, String password) {
+
+            mAuth.createUserwithEmailandPassword(email, password)
+                    .addOnCompleteListener(this,
+            if (task.isSuccessful()) {
+
+
+                Toast.makeText(SignUpActivity.this "User Registration Successful", Toast.LENGTH_SHORT).show();
+            }
+            else {
+
+                Toast.makeText(SignUpActivity.this "User Registration Failed", Toast.LENGTH_SHORT).show();
+
+            }
+
+        );
+
+
+
+        }
+
+
+
+    }
+
+
 }
