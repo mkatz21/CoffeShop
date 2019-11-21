362c304605dcb1249cf2375777b3c8f3d141998b
seleneceja <sceja@umich.edu>
2019-11-21 09:18:37 -0500

    Account and SignUp Activity updated pending final bug review v2

--- app/src/main/java/com/example/coffeshop/SignUpActivity.java
+++ app/src/main/java/com/example/coffeshop/SignUpActivity.java
@@ -1,8 +1,15 @@
 package com.example.coffeshop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 //test
 public class SignUpActivity extends AppCompatActivity {
 
@@ -11,4 +18,49 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mainmenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.MainPayment) {
+
+            Intent mainPaymentIntent = new Intent(this, MainPaymentActivity.class);
+            startActivity(mainPaymentIntent);
+
+        } else if(item.getItemId() == R.id.CheckIn) {
+
+            Toast.makeText(this, "You are already on the Check In Page", Toast.LENGTH_SHORT).show();
+
+        } else if(item.getItemId() == R.id.Account) {
+
+            Intent accountIntent = new Intent(this, AccountActivity.class);
+            startActivity(accountIntent);
+
+        } else if(item.getItemId() == R.id.AddPayment) {
+
+            Intent addPaymentIntent = new Intent(this, AddPaymentMethodActivity.class);
+            startActivity(addPaymentIntent);
+        } else if(item.getItemId() == R.id.Home) {
+
+            Intent HomeIntent = new Intent(this, HomeActivity.class);
+            startActivity(HomeIntent);
+        } else if(item.getItemId() == R.id.CoffeeShop) {
+
+            Intent coffeeShopIntent = new Intent(this, CoffeeShopActivity.class);
+            startActivity(coffeeShopIntent);
+        } else if(item.getItemId() == R.id.SignUp) {
+
+            Intent signupIntent = new Intent(this, SignUpActivity.class);
+            startActivity(signupIntent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
