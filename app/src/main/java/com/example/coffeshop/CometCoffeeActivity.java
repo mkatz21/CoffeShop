b749093197e7ea10b12a9af7c4ecf3c0607e45b3
Sam <epstein.sam@gmail.com>
2019-12-01 22:45:09 -0500

    SIE 12.1.19 Updating all of the coffee shop classes with menus + spinners

--- app/src/main/java/com/example/coffeshop/CometCoffeeActivity.java
+++ app/src/main/java/com/example/coffeshop/CometCoffeeActivity.java
@@ -1,14 +1,84 @@
 package com.example.coffeshop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class CometCoffeeActivity extends AppCompatActivity {
+    Spinner spinnerReservationDuration, spinnerReservationTableType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comet_coffee);
+
+        spinnerReservationDuration=findViewById(R.id.spinnerAvailabilityDuration);
+        spinnerReservationTableType = findViewById(R.id.spinnerAvailabilityTableType);
+
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapterDuration = ArrayAdapter.createFromResource(this,
+                R.array.reservation_duration_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapterDuration.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinnerReservationDuration.setAdapter(adapterDuration);
+
+        ArrayAdapter<CharSequence> adapterTableType = ArrayAdapter.createFromResource(this,
+                R.array.reservation_table_type_array, android.R.layout.simple_spinner_item);
+        adapterTableType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinnerReservationTableType.setAdapter(adapterTableType);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mainmenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.MainPayment) {
+
+            Intent mainPaymentIntent = new Intent(this, MainPaymentActivity.class);
+            startActivity(mainPaymentIntent);
+
+        } else if(item.getItemId() == R.id.CoffeeShop) {
+
+            Toast.makeText(this, "You are already on the Coffee Shop Page", Toast.LENGTH_SHORT).show();
+
+        } else if(item.getItemId() == R.id.Account) {
+
+            Intent accountIntent = new Intent(this, AccountActivity.class);
+            startActivity(accountIntent);
+
+        } else if(item.getItemId() == R.id.AddPayment) {
+
+            Intent addPaymentIntent = new Intent(this, AddPaymentMethodActivity.class);
+            startActivity(addPaymentIntent);
+        } else if(item.getItemId() == R.id.CheckIn) {
+
+            Intent checkInIntent = new Intent(this, CheckInActivity.class);
+            startActivity(checkInIntent);
+        } else if(item.getItemId() == R.id.Home) {
+
+            Intent homeIntent = new Intent(this, HomeActivity.class);
+            startActivity(homeIntent);
+        } else if(item.getItemId() == R.id.SignUp) {
+
+            Intent signupIntent = new Intent(this, SignUpActivity.class);
+            startActivity(signupIntent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
