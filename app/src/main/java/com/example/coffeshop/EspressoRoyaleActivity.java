18f6e9e7e8400753290e2cf99000da33127371cd
Amrit Rathi <amritr@umich.edu>
2019-12-06 12:48:47 -0500

    moved textviewCurrentPrice to different xml

--- app/src/main/java/com/example/coffeshop/EspressoRoyaleActivity.java
+++ app/src/main/java/com/example/coffeshop/EspressoRoyaleActivity.java
@@ -14,27 +14,49 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import org.w3c.dom.Text;
 
 import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
 
-public class EspressoRoyaleActivity extends AppCompatActivity implements View.OnClickListener {
-    Spinner spinnerReservationDuration, spinnerReservationTableType;
+public class EspressoRoyaleActivity extends AppCompatActivity implements View.OnClickListener,
+        RadioButton.OnCheckedChangeListener {
+    Spinner spinnerReservationDuration, spinnerReservationTableType, spinnerERtimeslots;
     TextView textViewCurrentPrice;
-    Button buttonAvailabilityTime1;
-
+    Button buttonAvailabilityTime1, buttonAvailabilityBookNow;
+    RadioButton radioButtonOwnTable, radioButtonSharedTable, radioButton30,radioButton1,radioButton1half,radioButton2;
 
     //Declare prices
     Double dblthirtyprice = 5.00;
-    Double dblhourprice = 10.00;
-    Double dblhourandhalfprice = 15.00;
-    Double dbltwohourprice = 20.00;
     Double dblshareddiscount = 3.00;
-    double newprice;
+    Double newprice;
+    Double oldprice=5.00;
+
+    //String ERTimeSlot1 = "10:00am";
+    //String ERTimeSlot2 = "10:30am";
+    //String ERTimeSlot3 = "11:00am";
+    //String ERTimeSlot4 = "11:30am";
+    //String ERTimeSlot5 = "12:00pm";
+    //String ERTimeSlot6 = "12:30pm";
+    //String ERTimeSlot7 = "1:00pm";
+    //String ERTimeSlot8 = "1:30pm";
+    //String ERTimeSlot9 = "2:00pm";
+    //String ERTimeSlot10 = "2:30pm";
+    //String ERTimeSlot11 = "3:00pm";
+    //String ERTimeSlot12 = "3:30pm";
+    //String ERTimeSlot13 = "4:00pm";
+    //String ERTimeSlot14 = "4:30pm";
+    //String ERTimeSlot15 = "5:00pm";
 
     NumberFormat formatter = NumberFormat.getCurrencyInstance();
 
@@ -48,83 +70,86 @@ public class EspressoRoyaleActivity extends AppCompatActivity implements View.On
         spinnerReservationDuration = findViewById(R.id.spinnerAvailabilityDuration);
         spinnerReservationTableType = findViewById(R.id.spinnerAvailabilityTableType);
 
-        buttonAvailabilityTime1 = findViewById(R.id.buttonAvailabilityTime1);
 
+        buttonAvailabilityTime1 = findViewById(R.id.buttonAvailabilityTime1);
+        buttonAvailabilityBookNow = findViewById(R.id.buttonAvailabilityBookNow);
+
+        radioButtonOwnTable = findViewById(R.id.radioButtonOwnTable);
+        radioButtonSharedTable = findViewById(R.id.radioButtonSharedTable);
+        radioButton30 = findViewById(R.id.radioButton30);
+        radioButton1 = findViewById(R.id.radioButton1);
+        radioButton1half = findViewById(R.id.radioButton1half);
+        radioButton2 = findViewById(R.id.radioButton2);
+
+        buttonAvailabilityBookNow.setOnClickListener(this);
         buttonAvailabilityTime1.setOnClickListener(this);
+        radioButtonOwnTable.setOnCheckedChangeListener(this);
+        radioButtonSharedTable.setOnCheckedChangeListener(this);
+        radioButton30.setOnCheckedChangeListener(this);
+        radioButton1.setOnCheckedChangeListener(this);
+        radioButton1half.setOnCheckedChangeListener(this);
+        radioButton2.setOnCheckedChangeListener(this);
+
+        spinnerERtimeslots = findViewById(R.id.spinnerERtimeslots);
+        ArrayAdapter<CharSequence> adapterERtimeslots = ArrayAdapter.createFromResource(this,R.array.timeslots, android.R.layout.simple_spinner_item);
+        adapterERtimeslots.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerERtimeslots.setAdapter(adapterERtimeslots);
 
         // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapterDuration = ArrayAdapter.createFromResource(this,
-                R.array.reservation_duration_array, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
-        adapterDuration.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //ArrayAdapter<CharSequence> adapterDuration = ArrayAdapter.createFromResource(this,
+                //R.array.reservation_duration_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        //adapterDuration.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         // Apply the adapter to the spinner
-        ArrayAdapter<CharSequence> adapterTableType = ArrayAdapter.createFromResource(this,
-                R.array.reservation_table_type_array, android.R.layout.simple_spinner_item);
-        adapterTableType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //ArrayAdapter<CharSequence> adapterTableType = ArrayAdapter.createFromResource(this,
+                //R.array.reservation_table_type_array, android.R.layout.simple_spinner_item);
+        //adapterTableType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         //This code is to change the checkout price as reservation table type changes
-        spinnerReservationTableType.setAdapter(adapterTableType);
+        //spinnerReservationTableType.setAdapter(adapterTableType);
 
         //This code is to change the checkout price as duration changes
-        spinnerReservationDuration.setAdapter(adapterDuration);
-        spinnerReservationDuration.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        //spinnerReservationDuration.setAdapter(adapterDuration);
+        //spinnerReservationDuration.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                int selectionCurrent = spinnerReservationDuration.getSelectedItemPosition();
-
-                if (selectionCurrent == 0) {
-                    textViewCurrentPrice.setText(formatter.format(dblthirtyprice));
-                } else if (selectionCurrent == 1) {
-                    textViewCurrentPrice.setText(formatter.format(dblhourprice));
-                } else if (selectionCurrent == 2) {
-                    textViewCurrentPrice.setText(formatter.format(dblhourandhalfprice));
-                } else if (selectionCurrent == 3) {
-                    textViewCurrentPrice.setText(formatter.format(dbltwohourprice));
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-                textViewCurrentPrice.setText(formatter.format(dblthirtyprice));
-            }
-        });
-
-        spinnerReservationTableType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                int selectiontabletype = spinnerReservationTableType.getSelectedItemPosition();
-                int selectionCurrent = spinnerReservationDuration.getSelectedItemPosition();
-                if (selectiontabletype == 1 & selectionCurrent == 0) {
-
-                    newprice = dblthirtyprice - dblshareddiscount;
-                    textViewCurrentPrice.setText(formatter.format(newprice));
-
-                } else if (selectiontabletype == 1 & selectionCurrent == 1) {
-
-                    newprice = dblhourprice - dblshareddiscount;
-                    textViewCurrentPrice.setText(formatter.format(newprice));
-
-                } else if (selectiontabletype == 1 & selectionCurrent == 2) {
-
-                    newprice = dblhourandhalfprice - dblshareddiscount;
-                    textViewCurrentPrice.setText(formatter.format(newprice));
-                } else if (selectiontabletype == 1 & selectionCurrent == 3) {
-
-                    newprice = dbltwohourprice - dblshareddiscount;
-                    textViewCurrentPrice.setText(formatter.format(newprice));
-
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
     }
 
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        if (radioButton30.isChecked()) {
+            UpdateCheckOutPrice();
+        } else if (radioButton1.isChecked()){
+            UpdateCheckOutPrice();
+        } else if (radioButton1half.isChecked()){
+            UpdateCheckOutPrice();
+        } else if (radioButton2.isChecked()){
+            UpdateCheckOutPrice();
+        }
+    }
+
+    public void UpdateCheckOutPrice() {
+        if (radioButtonOwnTable.isChecked() & radioButton30.isChecked()) {
+            newprice = oldprice*1;
+        } else if (radioButtonSharedTable.isChecked() & radioButton30.isChecked()){
+            newprice = oldprice-dblshareddiscount;
+        } else if (radioButtonOwnTable.isChecked() & radioButton1.isChecked()){
+            newprice = oldprice*2;
+        } else if (radioButtonSharedTable.isChecked() & radioButton1.isChecked()){
+            newprice = (oldprice)*2-dblshareddiscount;
+        } else if (radioButtonOwnTable.isChecked() & radioButton1half.isChecked()){
+            newprice = oldprice*3;
+        } else if (radioButtonSharedTable.isChecked() & radioButton1half.isChecked()){
+            newprice = (oldprice)*3-dblshareddiscount;
+        } else if (radioButtonOwnTable.isChecked() & radioButton2.isChecked()){
+            newprice = oldprice*4;
+        } else if (radioButtonSharedTable.isChecked() & radioButton2.isChecked()) {
+            newprice = (oldprice) * 4 - dblshareddiscount;
+        }
+
+        textViewCurrentPrice.setText(formatter.format(newprice));
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -175,7 +200,11 @@ public class EspressoRoyaleActivity extends AppCompatActivity implements View.On
             {
                 buttonAvailabilityTime1.setBackgroundColor(Color.GREEN);
             }
+        } else if (view == buttonAvailabilityBookNow) {
+            buttonAvailabilityTime1.setVisibility(View.INVISIBLE);
+
         }
     }
+
 
 }
