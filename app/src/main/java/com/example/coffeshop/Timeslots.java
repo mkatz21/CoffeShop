5e4e9ece92ae678d568bf3f248ab71b0a5f324c3
Amrit Rathi <amritr@umich.edu>
2019-12-06 12:50:04 -0500

    Worked with price calculation

---
+++ app/src/main/java/com/example/coffeshop/Timeslots.java
@@ -0,0 +1,15 @@
+package com.example.coffeshop;
+
+public class Timeslots {
+
+    public String timeslot;
+
+    public Timeslots() {
+    }
+
+    public Timeslots(String timeslot) {
+        this.timeslot = timeslot;
+    }
+}
+
+
