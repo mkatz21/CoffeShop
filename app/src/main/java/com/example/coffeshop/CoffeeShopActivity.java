7228a7a327476537982e26ad43353db2e419f9ff
Sam <epstein.sam@gmail.com>
2019-12-08 15:14:20 -0500

    SIE 12.8.19 Sending Duration, Table Type, and Price to AddPayment for Lab as a test

--- app/src/main/java/com/example/coffeshop/CoffeeShopActivity.java
+++ app/src/main/java/com/example/coffeshop/CoffeeShopActivity.java
@@ -30,7 +30,7 @@ import java.util.Calendar;
 public class CoffeeShopActivity extends AppCompatActivity implements View.OnClickListener,
         RadioButton.OnCheckedChangeListener {
 
-    String reservationCoffeeShop, reservationDate, reservationTime;
+    String reservationCoffeeShop, reservationDate, reservationTime, reservationDuration, reservationTable, reservationPrice;
     TextView textViewLab, textViewDate, textViewTime;
 
     Spinner spinnerLABtimeslots;
@@ -231,10 +231,14 @@ public class CoffeeShopActivity extends AppCompatActivity implements View.OnClic
     public void onClick(View view) {
         if (view == buttonAvailabilityBookNow){
           reservationCoffeeShop = textViewLab.getText().toString();
+          reservationPrice = textViewCurrentPrice.getText().toString();
             Intent reservationIntent = new Intent(this, AddPaymentMethodActivity.class);
             reservationIntent.putExtra("date", reservationDate);
             reservationIntent.putExtra("time", reservationTime);
             reservationIntent.putExtra("Coffee Shop", reservationCoffeeShop);
+            reservationIntent.putExtra("Duration", reservationDuration);
+            reservationIntent.putExtra("Table Type", reservationTable);
+            reservationIntent.putExtra("Price", reservationPrice);
             startActivity(reservationIntent);
 
         }
@@ -258,20 +262,36 @@ public class CoffeeShopActivity extends AppCompatActivity implements View.OnClic
     public void UpdateCheckOutPrice() {
         if (radioButtonOwnTable.isChecked() & radioButton30.isChecked()) {
             newprice = oldprice*1;
+            reservationDuration = "30 minutes";
+            reservationTable = "Own Table";
         } else if (radioButtonSharedTable.isChecked() & radioButton30.isChecked()){
             newprice = oldprice-dblshareddiscount;
+            reservationDuration = "30 minutes";
+            reservationTable = "Shared Table";
         } else if (radioButtonOwnTable.isChecked() & radioButton1.isChecked()){
             newprice = oldprice*2;
+            reservationDuration = "1 hour";
+            reservationTable = "Own Table";
         } else if (radioButtonSharedTable.isChecked() & radioButton1.isChecked()){
             newprice = (oldprice)*2-dblshareddiscount;
+            reservationDuration = "1 hour";
+            reservationTable = "Shared Table";
         } else if (radioButtonOwnTable.isChecked() & radioButton1half.isChecked()){
             newprice = oldprice*3;
+            reservationDuration = "1.5 hours";
+            reservationTable = "Own Table";
         } else if (radioButtonSharedTable.isChecked() & radioButton1half.isChecked()){
             newprice = (oldprice)*3-dblshareddiscount;
+            reservationDuration = "1.5 hours";
+            reservationTable = "Shared Table";
         } else if (radioButtonOwnTable.isChecked() & radioButton2.isChecked()){
             newprice = oldprice*4;
+            reservationDuration = "2 hours";
+            reservationTable = "Own Table";
         } else if (radioButtonSharedTable.isChecked() & radioButton2.isChecked()) {
             newprice = (oldprice) * 4 - dblshareddiscount;
+            reservationDuration = "2 hours";
+            reservationTable = "Shared Table";
         }
 
         textViewCurrentPrice.setText(formatter.format(newprice));
